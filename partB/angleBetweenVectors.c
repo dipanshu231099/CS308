20f74d686d77d92151a197c88bc27afe048ea5a4
hrs786 <52928171+hrs786@users.noreply.github.com>
2020-10-14 20:42:12 +0530

    partB: member E: function to find angle b/w vectors

--- partB/angleBetweenVectors.c
+++ partB/angleBetweenVectors.c
@@ -1,9 +1,10 @@
 #include "vectorlib.h"
 
 /* function to determine angle between two vectors in radians */
-float angleBetweenVectors(Vector a, Vector b){
-    float dot = dotProductVectors(a,b);
-    float modA = modulusVectors(a);
-    float modB = modulusVectors(b);
-    return dot/(modA*modB);
+float angleBetweenVectors(Vector a, Vector b){ // arguments - 2 vectors b/w which we have to find angle
+	/* angle = a.b/(|a||b|) */
+    float dotProduct = dotProductVectors(a,b);	// dot product between vec-a, vec-b
+    float modA = modulusVectors(a);	// len or mod of vec-a
+    float modB = modulusVectors(b);	// len or mod of vec-b
+    return dotProduct/(modA*modB);	// applying formula mentioned above
 }
